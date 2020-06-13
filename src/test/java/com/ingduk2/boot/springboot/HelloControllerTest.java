20e1cfd26b3f2391fa390ad156802020418b45fb
ingduk2 <ingduk2@naver.com>
2020-06-14 02:33:07 +0900

    5.7 Security Test

--- src/test/java/com/ingduk2/boot/springboot/HelloControllerTest.java
+++ src/test/java/com/ingduk2/boot/springboot/HelloControllerTest.java
@@ -1,23 +1,31 @@
 package com.ingduk2.boot.springboot;
 
+import com.ingduk2.boot.springboot.config.auth.SecurityConfig;
 import com.ingduk2.boot.springboot.web.HelloController;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class
+            ,excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello() throws Exception{
         String hello = "hello";
 
@@ -28,6 +36,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
